c015b233258fc9759d3c4c0640290526c8128f0d
UVBrain services <uvbrainsolutions@outlook.com>
2015-11-19 09:47:29 +0530

    draft

---
+++ src/app/cl-framework.ts
@@ -0,0 +1,15 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'cl-framework-app',
+  providers: [],
+  templateUrl: 'app/cl-framework.html',
+  directives: [],
+  pipes: []
+})
+export class ClFrameworkApp {
+
+  constructor() {}
+
+}
